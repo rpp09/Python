4d81012ed9e9f25a76abcc9495d0f2f69d2f90b0
rpp09 <rpascual21202@iesjoanramis.org>
2025-11-12 13:18:20 +0100

    Primer exercici de Python

---
+++ Prova1.py
@@ -0,0 +1,6 @@
+A = float(input("Escriure el primer nombre"))
+B = float(input("Escriure el segon nombre"))
+C = float(input("Escriure el tercer nombre"))
+print("El resultat de la suma {} + {} és {}".format(A, B, C, D))
+D = A * B * C
+print("El resultat de la multiplicació {} * {} és {}".format(A, B, C, D))
