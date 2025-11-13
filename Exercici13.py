5de9c96bb8e112b79ffe2a391af9da5012323a6e
rpp09 <rpascual21202@iesjoanramis.org>
2025-11-13 11:39:43 +0100

    Exercici 13

---
+++ Exercici13.py
@@ -0,0 +1,11 @@
+A= float(input("Insereix el primer nombre"))
+B= float(input("Insereix el segon nombre"))
+C= A + B
+D= A * B
+E= A / B
+F= A - B
+G= A % B
+print("La suma de {} + {} és igual a {}".format(A, B, C))
+print("La multiplicació de {} * {} és igual a {}".format(A, B, D))
+print("La divisió de {} i {} és igual a {}".format(A, B, E))
+print("La resta de {} i {} és igual a {}".format(A, B, F))
