e43a534c80289ff129642e41c787e4c82bce3fcb
rpp09 <rpascual21202@iesjoanramis.org>
2025-11-13 11:29:53 +0100

    Exercici 12

---
+++ Exercici12.py
@@ -0,0 +1,7 @@
+Edat= float(input("Insereix la seva edat"))
+if Edat>18:
+    print("Ets major d'edat, ficat a treballar")
+elif Edat<18:
+    print("Ets menor d'edat")
+else:
+    print("acabes de cumplir la majoria d'edat")    
