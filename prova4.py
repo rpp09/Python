40940bda88ffa3133bf21ec19bef80cef44c4d08
rpp09 <rpascual21202@iesjoanramis.org>
2025-11-24 09:41:15 +0100

    prova4

--- prova4.py
+++ prova4.py
@@ -1,9 +1,17 @@
+
+
+
+
+
+
+
+"""
 #a = [1, "a", "capça". [2], 1, "a"]
 a = [10, 9, 8, 7, 6, 5, 1, 2, 3, 4]
 print(a[::-1]) #Retorna una llista invertida, però no modifica l'original
 print(a)
 print(a.reverse())
-
+"""
 
 
 
