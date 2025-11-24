e6bc900f3ac3b26e8bb67c59370d11fa78b26299
rpp09 <rpascual21202@iesjoanramis.org>
2025-11-24 09:27:55 +0100

    prova4

---
+++ prova4.py
@@ -0,0 +1,19 @@
+#a = [1, "a", "capça". [2], 1, "a"]
+a = [10, 9, 8, 7, 6, 5, 1, 2, 3, 4]
+print(a[::-1]) #Retorna una llista invertida, però no modifica l'original
+print(a)
+print(a.reverse())
+
+
+
+
+""""
+a.sort()
+print(a)
+for i in range(len(a)):
+    a[i]=str(a[i])
+a.sort()
+print(a)
+"""
+
+print(a)
