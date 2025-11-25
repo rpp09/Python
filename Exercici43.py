aa4405aed4cdb3ec67da102230cc50a01bb56bfe
rpp09 <rpascual21202@iesjoanramis.org>
2025-11-25 09:43:48 +0100

    Ex43

---
+++ Exercici43.py
@@ -0,0 +1,8 @@
+def main():
+    for repeticio in range(5):
+        for numero in range(1, 16):
+            print(numero, end=" ")
+        print()  # Salta de línia després de cada sèrie
+
+if __name__ == "__main__":
+    main()
