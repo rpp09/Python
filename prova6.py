1afa59d535aee2ead6697bf6d84568af3b3e2bec
rpp09 <rpascual21202@iesjoanramis.org>
2025-11-26 12:34:49 +0100

    prova6

---
+++ prova6.py
@@ -0,0 +1,15 @@
+a = {"1":"H", "2":"o", "3":"l", "4":"a", "5":",", "6":[1, 2, 3, 4]}
+#b = a.pop("6")
+#b = a.popitem()
+b = {"7":"pepe", "8":"miquel"}
+print(b)
+print(a)
+
+
+"""
+for e in a:
+    print(a[e])
+
+for c,v in a.items():
+    print("{}:{},".format(c,v))
+"""
